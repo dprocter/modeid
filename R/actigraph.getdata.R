44e74795b809268ece863a70cedee61d264d0025
Duncan Procter <dprocter@gmail.com>
2017-08-25 13:55:42 +0100

    actigraph unlabelled axes
    no axis labels from the actigraph files caused an error

--- R/actigraph.getdata.R
+++ R/actigraph.getdata.R
@@ -6,6 +6,18 @@ actigraph.getdata<-function(accfile, epoch.length){
   start.datetime<-strptime(start.datetime,format="%d/%m/%Y %H:%M:%S")
 
   acc.data<-read.csv(accfile,skip=10)
+  if (length(acc.data)==3){
+    names(acc.data)[1:3]<-c("Axis1","Axis2","Axis3")
+  } else{
+    if (length(acc.data)==2){
+      names(acc.data)[1:2]<-c("Axis1","Axis2")
+    } else{
+      if (length(acc.data)==1){
+        names(acc.data)[1]<-"Axis1"
+      }
+    }
+  }
+
   acc.data$date.time<-seq(start.datetime, start.datetime+(length(acc.data$Axis1)-1)*epoch.length, epoch.length)
 
   return(acc.data)
