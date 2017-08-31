6ef3f7afb4822ffbd1f3d70b801cb42ab52f5e2c
Duncan Procter <dprocter@gmail.com>
2017-08-31 09:26:56 +0100

    mode
    now reads in any actilife processed epoch files

--- R/actigraph.getdata.R
+++ R/actigraph.getdata.R
@@ -1,22 +1,53 @@
 actigraph.getdata<-function(accfile, epoch.length){
+
+  mode.df<-data.frame(seq(0,63,1))
+  names(mode.df)<-"mode"
+  mode.df$Axis1<-1
+  mode.df$Axis2<-rep(c(0,1),each=4,times=8)
+  mode.df$Axis3<-rep(c(0,1),each=8,times=4)
+  mode.df$steps<-rep(c(0,1),times=32)
+  mode.df$HR<-rep(c(0,1),each=2,times=16)
+  mode.df$Lux<-rep(c(0,1),each=16,times=2)
+  mode.df$Incline<-rep(c(0,1),each=32)
+
   metadata<-read.csv(accfile,nrows=8)
+  mode.reported<-as.numeric(strsplit(as.character(metadata[8,]),split=" ")[[1]][11])
   start.date<-strsplit(as.character(metadata[3,]),split=" ")[[1]][3]
   start.time<-strsplit(as.character(metadata[2,]),split=" ")[[1]][3]
   start.datetime<-paste(start.date,start.time)
   start.datetime<-strptime(start.datetime,format="%d/%m/%Y %H:%M:%S")
 
+  mode.df<-subset(mode.df,mode==mode.reported)
+
   acc.data<-read.csv(accfile,skip=10)
-  if (length(acc.data)==3){
-    names(acc.data)[1:3]<-c("Axis1","Axis2","Axis3")
-  } else{
-    if (length(acc.data)==2){
-      names(acc.data)[1:2]<-c("Axis1","Axis2")
-    } else{
-      if (length(acc.data)==1){
-        names(acc.data)[1]<-"Axis1"
-      }
-    }
+  names(acc.data)[1]<-"Axis1"
+  col.marker<-1
+
+  if (mode.df$Axis2[1]==1){
+    col.marker<-col.marker+1
+    names(acc.data)[col.marker]<-"Axis2"
   }
+  if (mode.df$Axis3[1]==1){
+    col.marker<-col.marker+1
+    names(acc.data)[col.marker]<-"Axis3"
+  }
+  if (mode.df$steps[1]==1){
+    col.marker<-col.marker+1
+    names(acc.data)[col.marker]<-"steps"
+  }
+  if (mode.df$HR[1]==1){
+    col.marker<-col.marker+1
+    names(acc.data)[col.marker]<-"HR"
+  }
+  if (mode.df$Lux[1]==1){
+    col.marker<-col.marker+1
+    names(acc.data)[col.marker]<-"Lux"
+  }
+  if (mode.df$Incline[1]==1){
+    col.marker<-col.marker+1
+    names(acc.data)[col.marker:(col.marker+3)]<-c("inc.off","inc.stand","inc.sit","inc.lie")
+  }
+
 
   acc.data$date.time<-seq(start.datetime, start.datetime+(length(acc.data$Axis1)-1)*epoch.length, epoch.length)
 
