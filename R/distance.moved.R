d45353a4d72d76cec7c35194f473a88b2de4d145
Duncan Procter <dprocter@gmail.com>
2017-09-08 16:03:21 +0100

    distance moved
    finally made this into a function

---
+++ R/distance.moved.R
@@ -0,0 +1,31 @@
+#' @export
+#' @title Distance moved
+#' @description a function that measures distance moved between points separated by a time window
+#'
+
+
+distance.moved<-function(dataset, last, time.window, epoch.length){
+  this.data<-dataset
+  epochs.shifted<-time.window/epoch.length
+
+  if (isTRUE(last)){
+    this.data$east.shift<-NA
+    this.data$east.shift[1:(length(this.data[,1])-epochs.shifted)]<-this.data$easting[
+      epochs.shifted+1:length(this.data[,1])]
+    this.data$north.shift<-NA
+    this.dataa$north.shift[1:(length(this.data[,1])-epochs.shifted)]<-this.data$northing[
+      epochs.shifted+1:length(this.data[,1])]
+  } else{
+    this.data$east.shift<-NA
+    this.data$east.shift[epochs.shifted+1:length(merged.data[,1])]<-merged.data$easting[
+      1:(length(merged.data[,1])-epochs.shifted)]
+    this.data$north.shift<-NA
+    this.data$north.shift[epochs.shifted+1:length(merged.data[,1])]<-merged.data$northing[
+      1:(length(merged.data[,1])-epochs.shifted)]
+  }
+
+  dist.moved<-sqrt(
+    (this.data$easting-this.data$east.shift)^2+(this.data$northing-this.data$north.shift)^2)
+
+  reurn(dist.moved)
+}
