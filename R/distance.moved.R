7cc108fee9b91c856a1ebb8046d927b54a7c47fe
Duncan Procter <dprocter@gmail.com>
2017-09-11 12:09:26 +0100

    dist moved fix
    poor bracketing fixed

--- R/distance.moved.R
+++ R/distance.moved.R
@@ -11,21 +11,21 @@ distance.moved<-function(dataset, last, time.window, epoch.length){
   if (isTRUE(last)){
     this.data$east.shift<-NA
     this.data$east.shift[1:(length(this.data[,1])-epochs.shifted)]<-this.data$easting[
-      epochs.shifted+1:length(this.data[,1])]
+      (epochs.shifted+1):length(this.data[,1])]
     this.data$north.shift<-NA
-    this.dataa$north.shift[1:(length(this.data[,1])-epochs.shifted)]<-this.data$northing[
-      epochs.shifted+1:length(this.data[,1])]
+    this.data$north.shift[1:(length(this.data[,1])-epochs.shifted)]<-this.data$northing[
+      (epochs.shifted+1):length(this.data[,1])]
   } else{
     this.data$east.shift<-NA
-    this.data$east.shift[epochs.shifted+1:length(merged.data[,1])]<-merged.data$easting[
-      1:(length(merged.data[,1])-epochs.shifted)]
+    this.data$east.shift[(epochs.shifted+1):length(this.data[,1])]<-this.data$easting[
+      1:(length(this.data[,1])-epochs.shifted)]
     this.data$north.shift<-NA
-    this.data$north.shift[epochs.shifted+1:length(merged.data[,1])]<-merged.data$northing[
-      1:(length(merged.data[,1])-epochs.shifted)]
+    this.data$north.shift[(epochs.shifted+1):length(this.data[,1])]<-this.data$northing[
+      1:(length(this.data[,1])-epochs.shifted)]
   }
 
   dist.moved<-sqrt(
     (this.data$easting-this.data$east.shift)^2+(this.data$northing-this.data$north.shift)^2)
 
-  reurn(dist.moved)
+  return(dist.moved)
 }
