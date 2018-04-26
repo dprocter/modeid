507b8f1160d52e33bf05c9e62a9cb1d15ccd59a4
Duncan Procter <dprocter@gmail.com>
2018-04-26 11:01:40 +0100

    foldpred
    foldpred

---
+++ R/foldpred.R
@@ -0,0 +1,31 @@
+#' @export
+#' @title Predict modal travel mode from cross-validated model
+#' @description Returns a factor of the length of the data with predicted travel mode
+#' @param data
+#' The data to predict to, make sure the correct variables are selected using \code{\link{pred.data}}
+#' @param folds
+#'The number of folds in the data i.e. if it is 5-fold CV, then 5
+#' @param model
+#' The cross-validated model, created with \code{\link{cross.validator}}
+#' @return
+#' A factor variable of the same length as variables in the data, with predicted travel mode
+
+##########################
+foldpred<-function(data, folds, model){
+  
+  Mode <- function(x) {
+    ux <- unique(x)
+    ux[which.max(tabulate(match(x, ux)))]
+  }
+  
+  allpreds<-data.frame(numeric(length(data[,1])))
+  for (i in 1:folds){
+    pred<-predict(model[,1][[i]], newdata=as.matrix(modeid::pred.data(data)))
+    pred<-factor(pred, labels = c("cycle","stat","train","vehicle","walk"))
+    allpreds<-cbind(allpreds,pred)
+  }
+  
+  modal.mode<-apply(allpreds[2:length(allpreds)],1,FUN=Mode)
+  modal.mode<-factor(modal.mode)
+  return(modal.mode)
+}
